3b58e94e9b4ba72b479884675de83bb67cbab635
DTaraniuk <dimataraniuk@gmail.com>
2023-05-01 19:55:54 +0200

    rollback debug

--- src/Invert.java
+++ src/Invert.java
@@ -5,17 +5,7 @@ public class Invert implements AM {
     public void run(AMInfo amInfo) {
         //input
         int pivotNum = amInfo.parent.readInt();
-        if (pivotNum < 0) {
-            System.out.println("Error: pivotNum is negative. Please provide a valid index.");
-            return;
-        }
-
         double[][] matrix = (double[][])amInfo.parent.readObject();
-        if (pivotNum >= matrix.length) {
-            System.out.println("Error: pivotNum is greater than or equal to the number of rows in the matrix. Please provide a valid index.");
-            return;
-        }
-
         double[] pivotRow = (double[])amInfo.parent.readObject();
 
         double pivot = pivotRow[pivotNum];
